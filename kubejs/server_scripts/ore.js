e4fbf6b144ce3e775c56a4fb985c10f1f0b16c14
nvaz <memset114514@126.com>
2026-05-06 12:26:59 +0800

    第一章任务完成

--- kubejs/server_scripts/ore.js
+++ kubejs/server_scripts/ore.js
@@ -1,8 +1,9 @@
 ServerEvents.recipes(event => {
      event.remove({ type: 'minecraft:smelting' })
      event.remove({ type: 'minecraft:blasting' })
+     event.remove({ type: 'charcoal_pit:orekiln' })
+     event.remove({type:'charcoal_pit:bloomery'})
 
-     //event.remove({ type: 'charcoal_pit:orekiln' })
      event.custom({
           type: 'charcoal_pit:orekiln',
           ingredients: [
@@ -30,6 +31,15 @@ ServerEvents.recipes(event => {
                item: 'gtceu:tin_nugget', count: 3
           }
      })
+
+     //wrought_iron
+    event.custom({
+        type:'charcoal_pit:bloomery',
+        input:{tag:'forge:ores/iron'},
+        output:{item:'gtceu:wrought_iron_ingot'},
+        fail:{item:'charcoal_pit:bloom_fail'},
+        cool:{item:'charcoal_pit:bloom_cool'}
+    })
 })
 ServerEvents.tags('item', event=>{
      event.add('gtte:orekiln_copper', '#forge:ores/copper', 'minecraft:raw_copper')
--- kubejs/server_scripts/recipe.js
+++ kubejs/server_scripts/recipe.js
@@ -1,3 +1,30 @@
 ServerEvents.recipes(event=>{
-    
+    event.remove([{type:'gtceu:alloy_smelter'}, {type:'gtceu:alloy_blast_smelter'}, {type:'gtceu:arc_furnace'}, {type:'gtceu:extractor'}, {type:'gtceu:electric_blast_furnace'}])
+
+    event.remove({type:'crafting_shapeless', output:'charcoal_pit:unfired_sandy_brick'})
+    event.shapeless('4x charcoal_pit:unfired_sandy_brick', [
+        'minecraft:sand',
+        '4x minecraft:clay_ball',
+        'gtceu:brick_wooden_form'
+    ]).keepIngredient('gtceu:brick_wooden_form')
+
+    event.shaped('charcoal_pit:main_bloomery', [
+        'SPS',
+        'SAS',
+        'SPS'
+    ], {
+        S:'gtceu:bronze_rod',
+        P:'gtceu:bronze_plate',
+        A:'charcoal_pit:sandy_brick'
+    })
+
+    event.remove({output:'charcoal_pit:sandy_tuyere'})
+    event.shaped('charcoal_pit:sandy_tuyere', [
+        ' S ',
+        'SAS',
+        ' S '
+    ], {
+        S:'gtceu:bronze_rod',
+        A:'charcoal_put:sandy_brick'
+    })
 })
